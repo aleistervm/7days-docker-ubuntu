b89aff82f87f6e062e067071ff1af5203e72b5c3
Aleister V.M <113066551+aleistervm@users.noreply.github.com>
2023-09-11 23:16:24 -0500

    run everything in screen

--- install_7days.sh
+++ install_7days.sh
@@ -3,14 +3,14 @@
 # Switch to /gameserver
 cd /gameserver || exit
 
-# Create or update the 7 Days to Die server files in the mounted volume
-./steamcmd.sh +force_install_dir /7days +login anonymous +app_update 294420 validate +quit
+# Start a screen session named "7days-server"
+screen -dmS 7days-server
 
-# Copy in our pre-defined server config
-cp serverconfig.xml /7days
+# Attach to the screen session
+screen -S 7days-server -X stuff $'./steamcmd.sh +force_install_dir /7days +login anonymous +app_update 294420 validate +quit\n'
+screen -S 7days-server -X stuff $'cp serverconfig.xml /7days\n'
+screen -S 7days-server -X stuff $'cd /7days\n'
+screen -S 7days-server -X stuff $'./startserver.sh -configfile=serverconfig.xml\n'
 
-# Switch to /gameserver/7days
-cd /7days || exit
-
-# Start your server
-screen -dmS days7server ./startserver.sh -configfile=serverconfig.xml
+# Detach from the screen session
+screen -S 7days-server -X detach
