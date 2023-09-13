2ebf9494d5818918b92cd551247354ac26930d45
Aleister V.M <113066551+aleistervm@users.noreply.github.com>
2023-09-13 15:39:39 -0500

    more on FTP

--- ftp-server/user.sh
+++ ftp-server/user.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+echo "Starting vsftpd..."
+
 # Start the FTP server
 # /etc/init.d/vsftpd start
 /usr/sbin/vsftpd /etc/vsftpd.conf
 
 # Check the exit status of the previous command
-if [ $? -ne 0 ]; then
+if [ $? -ne 0 ]
+then
   # An error occurred, so let's check the logs
   echo "vsftpd failed to start. Checking logs..."
 
